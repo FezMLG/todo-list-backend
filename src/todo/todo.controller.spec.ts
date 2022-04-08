4c3ee92fdb1d20d735549974a81ce508e02ed4c7
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-04-08 13:44:38 +0200

    updating controller test to pass

--- src/todo/todo.controller.spec.ts
+++ src/todo/todo.controller.spec.ts
@@ -1,13 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoController } from './todo.controller';
+import { TodoService } from './todo.service';
 
 describe('TodoController', () => {
   let controller: TodoController;
 
+  const mockTodoService = {
+    setItem: jest.fn(),
+    removeItem: jest.fn(),
+    getAllItems: jest.fn(),
+    setItemStatus: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodoController],
-    }).compile();
+      providers: [TodoService],
+    })
+      .overrideProvider(TodoService)
+      .useValue(mockTodoService)
+      .compile();
 
     controller = module.get<TodoController>(TodoController);
   });
